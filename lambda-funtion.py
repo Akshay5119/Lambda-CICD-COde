d96c68d7c218d0eff15f421ac3eb38f516c1dbaf
Chandrashekharduble <72442589+Chandrashekharduble@users.noreply.github.com>
2022-06-23 14:46:14 +0530

    Update lambda-funtion.py

--- lambda-funtion.py
+++ lambda-funtion.py
@@ -1,8 +1,8 @@
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!  !....... (Modified v0.2'),
-    };
-    return response;
-};
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!....... (Modified v0.2')
+    }
